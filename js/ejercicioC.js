bff42c2cf9dc4930f5f914ba4c9c890e4e273c7a
devoto2003@gmail.com <devoto2003@gmail.com>
2023-07-27 22:16:20 -0300

    copia b  commit

---
+++ js/ejercicioC.js
@@ -0,0 +1,13 @@
+var cantidadDeGatos = 5;
+var cantidadDePasos = 3;
+
+console.log("");
+console.log("Ejercicio c");
+for(i=1; i<=cantidadDeGatos ; i++){
+    var pasos="";
+    for(j=1;  j<=cantidadDePasos; j++){
+        pasos += "🐾";
+    }
+    console.log("Gato #" + i + ": 🐈 " + pasos);
+    
+}
