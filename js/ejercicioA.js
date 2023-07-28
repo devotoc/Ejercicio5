a9ae6f22aa5d40bb4b4e30a55f8b965dcc7cd808
devoto2003@gmail.com <devoto2003@gmail.com>
2023-07-27 22:07:27 -0300

    primer  commit

---
+++ js/ejercicioA.js
@@ -0,0 +1,11 @@
+var cantidadDeGatos = 10;
+console.log("Ejercicio a");
+for( i=1 ; i<=cantidadDeGatos; i++){
+    if (i==1,4,7,10) {
+        console.log("Gato #"+i+ ": 😺");
+    }
+    else if (i==2,5,9){
+        console.log("Gato #"+i+ ": 😸");
+    }
+    else console.log("Gato #"+i+ ": 😹");
+}
